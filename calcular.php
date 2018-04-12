5bbe101363cb069e5248ebf8dacb10351df2680f
charlesandreatta <charlesandreatta@hotmail.com>
2018-04-12 02:20:34 -0300

    final
    calculadora em php

---
+++ calcular.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+
+		
+		function validar($valor1, $valor2){
+	return is_numeric($valor1) &&  is_numeric($valor2);
+	}
+		
+		function Soma($valor1, $valor2){
+	return $valor1 + $valor2;
+	}
+
+		function Dividir($valor1, $valor2){
+	return $valor2 !=0 ? $valor1 / $valor2: "divisão por zero";
+	}
+		
+		function Subtrair($valor1, $valor2){
+	return $valor1 - $valor2;
+	}
+	
+	function Multiplicar($valor1, $valor2){
+	return $valor1 * $valor2;
+	}
+		
+		
+	echo $Resultado;
+	
+?>
